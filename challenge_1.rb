dbd97cb5c8e46bc8875fd6b81cb12a8bfa7d2409
alex greenwood <alexander.greenwood@gmail.com>
2014-04-26 10:48:53 -0500

    Cleaned up the notes

--- challenge_1.rb
+++ challenge_1.rb
@@ -14,32 +14,11 @@
 #   http://en.wikipedia.org/wiki/Mortgage_calculator
 
 def pmt(rate, nper, pv)
-#   # ============================================
-#   # Your code to implement the method goes here.
-#   # ============================================
 
 ((rate/12) * pv)/(1 - ((1 + (rate/12))**(-nper*12)))
-# = ((6.5 / 100 / 12) * 200000) / (1 - ((1 + (6.5 / 100 / 12)) ^ (-30 * 12)))
+
 end
 
 puts "Your monthly payment will be #{pmt(0.065, 30, 200000)}."
 
-# puts "what is your interest rate?"
-# gets.chomp.to_f
 
-# puts "how many years on the loan?"
-# gets.chomp.to_i
-
-# puts "what is the principal value of your loan?"
-# gets.chomp.to_i
-
-
-# pmt(0.04, 12, 100000)
-
-  # puts "Your monthly payment will be #{pmt(0.01, 60, 30000)}."
-# = PMT(rate/12, 30 * 12, 200000)
-
-
-# Example usage of the method:
-
-# puts "Your monthly payment will be #{pmt(0.01, 60, 30000)}."
